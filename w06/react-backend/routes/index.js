560ece998bc4c946d26c1312d8070dfe7e3cee8f
zhps30149 <zhps30149@gmail.com>
2018-10-22 22:58:00 +0800

    delete w06

--- w06/react-backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { id: '405410324', name: '呂丞凱' });
-});
-
-module.exports = router;
